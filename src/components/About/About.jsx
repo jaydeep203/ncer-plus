627d376e45f99a28098c7bccde8865d6520b9bda
Jaydeep Deshpande <mrjaydeep2003@gmail.com>
2024-05-26 22:30:15 +0530

    6th semester

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -14,11 +14,13 @@ const About = () => {
         </p>
         <br />
         <p> 
-            This web is the web version of the NCER+ apk which is previously developed by our college students. I have taken inspiration from the NCER+ apk. I have gave my best in developing in this web app there is also some bugs I will try to fix it later.
+          About...
+            {/* This web is the web version of the NCER+ apk which is previously developed by our college students. I have taken inspiration from the NCER+ apk. I have gave my best in developing in this web app there is also some bugs I will try to fix it later. */}
         </p>
         <br />
         <p>If you have some doubts contact me at this <ArrowRight/></p>
-        <a  href="mailto:djtricks2003@gmail.com"><button id="submit">Contact Me</button></a>
+        {/* <a  href="mailto:djtricks2003@gmail.com"><button id="submit">Contact Me</button></a> */}
+        <a  href="www.google.com"><button id="submit">Contact Me</button></a>
         
       </div>
     </div>
--- src/components/ForPracticles/ForPracticles.jsx
+++ src/components/ForPracticles/ForPracticles.jsx
@@ -3,14 +3,19 @@ import Card from '../Cards/Card'
 const ForPracticles = () => {
 
     const object = [
+        // {
+        //     subj:"python",
+        //     link:"https://www.google.com"
+        // },
+        // {
+        //     subj:"OS",
+        //     link:"https://drive.google.com/drive/folders/1F8_QipFo4SM80ASghmFCrehhTEW2SuXR?usp=drive_link"
+        // },
         {
-            subj:"python",
-            link:"https://www.google.com"
+            subj:"Competitve Programming(CP)",
+            link:"https://veiled-tarragon-c59.notion.site/6th-Semester-Practicals-5d5a209a28554f30957640a337ba9b7c"
         },
-        {
-            subj:"OS",
-            link:"https://drive.google.com/drive/folders/1F8_QipFo4SM80ASghmFCrehhTEW2SuXR?usp=drive_link"
-        }
+        
 
     ];
 
