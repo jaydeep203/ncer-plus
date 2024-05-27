08ddea74cf50ba0d6bff42aa5fd3516a25522a10
Jaydeep Deshpande <mrjaydeep2003@gmail.com>
2024-05-28 01:23:53 +0530

    ML practicals

--- src/components/ForPracticles/ForPracticles.jsx
+++ src/components/ForPracticles/ForPracticles.jsx
@@ -15,6 +15,10 @@ const ForPracticles = () => {
             subj:"Competitve Programming(CP)",
             link:"https://veiled-tarragon-c59.notion.site/6th-Semester-Practicals-5d5a209a28554f30957640a337ba9b7c"
         },
+        {
+            subj:"Machine Learning(ML)",
+            link:"https://veiled-tarragon-c59.notion.site/ML-49c6786fb14b49d9b9f77bd5dd561398"
+        },
         
 
     ];
